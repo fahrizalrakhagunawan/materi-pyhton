fcfc16d300def33ba75f808767fa52bfaf73a860
fahrizalrakhagunawan <agoentea30@gmail.com>
2024-11-20 11:43:27 +0700

    menambahkan 2 file

---
+++ latihan/looping/latihan3.py
@@ -0,0 +1,8 @@
+# program untuk menampilkan data angka sebanyak 10 kali menggunakan for
+
+# for haris in range(10):
+#     print(haris)
+
+    # menampilkan angka genap dari 1 sampai 100
+for adem in range(2,101,2):
+     print(adem)
